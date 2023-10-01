2c2c1e9874787d165d2d47a776b3ae503409cfc4
fitriyanuralyashifa <fitriyaalya24@gmail.com>
2023-10-02 00:41:52 +0700

    TP01

---
+++ TP03.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TP03 {
+    
+    public static void main(final String[] args) {
+        // Kerjakan soalnya di sini
+        Scanner input = new Scanner (System.in);
+        int jumlahPermen = input.nextInt();
+        int jumlahTeman = input.nextInt();
+
+        int jatahPermen = jumlahPermen / (jumlahTeman + 1);
+        int sisaPermen = jumlahPermen % (jumlahTeman + 1);
+
+        System.out.println(jatahPermen);
+        System.out.println(sisaPermen);
+
+
+    }
+}
